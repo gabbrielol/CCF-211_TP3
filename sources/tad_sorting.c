8b449847b2235cacc4c2396f61eec94c930a22b1
Gabriel Rodrigues Marques <gabriel.r.marques@ufv.br>
2022-12-08 15:56:03 -0300

    initial commit

--- sources/tad_sorting.c
+++ sources/tad_sorting.c
@@ -1,11 +1,11 @@
 #include "../headers/tad_sorting.h"
 
 void bubble_sort(new_dicionario *new_dicionario, int length, int num_letra) {
-    int com; com = 0;
-    int mov; mov = 0;
     int i, j;
     char *aux;
 
+    int com; com = 0;
+    int mov; mov = 0;
     clock_t start, end;
     time_t t_start, t_end;
     double cpu_time_used;
@@ -26,6 +26,51 @@ void bubble_sort(new_dicionario *new_dicionario, int length, int num_letra) {
                 mov++;
             }
         }
+    }
+
+    end = clock();
+    t_end = time(NULL);
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    time_used = difftime(t_end, t_start);
+
+    printf("\n");
+    printf("Tempo (método clock_t): %lf\n", cpu_time_used);
+    printf("Tempo (método time_t): %lf\n", time_used);
+    printf("Comparações: %d\n", com);
+    printf("Movimentações: %d\n", mov);
+    printf("\n");
+    printf("-----------------------------------------------------------------\n");
+}
+
+void selection_sort(new_dicionario *new_dicionario, int length, int num_letra) {
+    int i, j;
+    int min;
+    char *aux;
+
+    int com; com = 0;
+    int mov; mov = 0;
+    clock_t start, end;
+    time_t t_start, t_end;
+    double cpu_time_used;
+    double time_used;
+
+    start = clock();
+    t_start = time(NULL);
+
+    for (i = 0; i < length - 1; i++) {
+        min = i;
+        for (j = i + 1; j < length; j++) {
+            com++;
+            if (strcmp(new_dicionario -> alfabeto[num_letra].lista_palavras_vetor.vetor_palavras[j], 
+            new_dicionario -> alfabeto[num_letra].lista_palavras_vetor.vetor_palavras[min]) < 0) {
+                min = j;
+            }
+        }
+        strcpy(aux, new_dicionario -> alfabeto[num_letra].lista_palavras_vetor.vetor_palavras[min]);
+        strcpy(new_dicionario -> alfabeto[num_letra].lista_palavras_vetor.vetor_palavras[min],
+        new_dicionario -> alfabeto[num_letra].lista_palavras_vetor.vetor_palavras[i]);
+        strcpy(new_dicionario -> alfabeto[num_letra].lista_palavras_vetor.vetor_palavras[i], aux);
+        mov++;
     }
 
     end = clock();
